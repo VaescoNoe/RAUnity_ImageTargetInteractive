e0351baec7e9373ff9ea8cd7002c02653dec061f
Noe <VaescoNoe@gmail.com>
2019-05-22 23:44:49 -0500

    Image target interative

---
+++ Assets/Colision.cs
@@ -0,0 +1,64 @@
+﻿
+using UnityEngine;
+using Vuforia;
+
+public class Colision : MonoBehaviour, ITrackableEventHandler
+{
+
+    protected TrackableBehaviour mTrackableBehaviour;
+    public MeshRenderer esfera;
+    public MeshRenderer cubo;
+    public Material rojo, verde;
+
+
+
+    public void OnTrackableStateChanged(
+    TrackableBehaviour.Status previousStatus,
+    TrackableBehaviour.Status newStatus)
+    {
+      
+
+        if (newStatus == TrackableBehaviour.Status.DETECTED ||
+            newStatus == TrackableBehaviour.Status.TRACKED ||
+            newStatus == TrackableBehaviour.Status.EXTENDED_TRACKED)
+        {
+           
+        }
+        else if (previousStatus == TrackableBehaviour.Status.TRACKED &&
+                 newStatus == TrackableBehaviour.Status.NO_POSE)
+        {
+           
+        }
+        else
+        {
+          
+        }
+    }
+
+    // Start is called before the first frame update
+    protected virtual void Start()
+    {
+        mTrackableBehaviour = GetComponent<TrackableBehaviour>();
+        if (mTrackableBehaviour)
+            mTrackableBehaviour.RegisterTrackableEventHandler(this);
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "verde")
+        {
+            esfera.material = verde;
+            cubo.material = rojo;
+        }
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.tag == "verde")
+        {
+            esfera.material = rojo;
+            cubo.material = verde;
+        }
+
+    }
+}
